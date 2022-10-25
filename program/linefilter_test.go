61579018984ccc84bcaf8fe3da3b1553a1f87cbf
Dewey Sasser <dsasser@actbluetech.com>
2022-10-25 17:26:48 -0400

    test: add line filter tests

--- program/linefilter_test.go
+++ program/linefilter_test.go
@@ -3,6 +3,7 @@ package program
 import (
 	"github.com/deweysasser/cumulus/cumulus"
 	"github.com/stretchr/testify/assert"
+	"strings"
 	"testing"
 )
 
@@ -52,4 +53,90 @@ func TestParseFilter(t *testing.T) {
 	assert.NoError(t, err)
 
 	assert.False(t, exp(fields))
+}
+
+func TestJoinedExpressions(t *testing.T) {
+
+	tests := []struct {
+		name   string
+		args   string
+		fields []string
+
+		want bool
+	}{
+		{"single expression, single field", "a=alpha", []string{"a=alpha"}, true},
+		{"single expression, single field", "a=alpha", []string{"a=alpha", "b=beta"}, true},
+		{"single expression, single field", "a=alpha", []string{"a=something"}, false},
+
+		{"single expression, single field", "a=alpha,b=beta", []string{"a=alpha"}, false},
+		{"single expression, single field", "a=alpha,b=beta", []string{"a=alpha", "b=beta"}, true},
+		{"single expression, single field", "a=alpha,b=beta", []string{"a=alpha", "b=gamma"}, false},
+
+		{"single expression, single field", "tag:Environment=staging,region=us-east-2", []string{"tag:Environment=staging", "region=us-east-2"}, true},
+		{"single expression, single field", "tag:Environment=staging,region=us-east-2", []string{"tag:Environment=staging", "region=us-east-1"}, false},
+
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ParseFilter(tt.args)
+			assert.NoError(t, err)
+
+			fields := make(cumulus.Fields)
+			for _, f := range tt.fields {
+				ss := strings.Split(f, "=")
+				fields[cumulus.FieldMeta{
+					Kind:          cumulus.WHERE,
+					Name:          ss[0],
+					DefaultHidden: false,
+				}] = cumulus.NewFieldValue(ss[1])
+			}
+
+			assert.Equalf(t, tt.want, got(fields), "ParseFilter(%v) on %v", tt.args, tt.fields)
+		})
+	}
+}
+
+func TestMultipleExpressions(t *testing.T) {
+
+	tests := []struct {
+		name   string
+		args   []string
+		fields []string
+
+		want bool
+	}{
+		{"single expression, single field", []string{"a=alpha"}, []string{"a=alpha"}, true},
+		{"single expression, single field", []string{"a=alpha"}, []string{"a=alpha", "b=beta"}, true},
+		{"single expression, single field", []string{"a=alpha"}, []string{"a=something"}, false},
+
+		{"single expression, single field", []string{"a=alpha,b=beta"}, []string{"a=alpha"}, false},
+		{"single expression, single field", []string{"a=alpha,b=beta"}, []string{"a=alpha", "b=beta"}, true},
+		{"single expression, single field", []string{"a=alpha,b=beta"}, []string{"a=alpha", "b=gamma"}, false},
+
+		{"single expression, single field", []string{"a=alpha,b=beta", "b=gamma"}, []string{"a=alpha", "b=gamma"}, true},
+		{"single expression, single field", []string{"a=alpha,b=beta", "a=delta, b=gamma"}, []string{"a=delta", "b=gamma"}, true},
+		{"single expression, single field", []string{"a=alpha,b=beta", "a=delta, b=gamma"}, []string{"a=delta", "b=gamma"}, true},
+		{"single expression, single field", []string{"a=alpha,b=beta", "a=delta, b=gamma"}, []string{"a=delta", "b=episilon"}, false},
+
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ParseFilters(tt.args)
+			assert.NoError(t, err)
+
+			fields := make(cumulus.Fields)
+			for _, f := range tt.fields {
+				ss := strings.Split(f, "=")
+				fields[cumulus.FieldMeta{
+					Kind:          cumulus.WHERE,
+					Name:          ss[0],
+					DefaultHidden: false,
+				}] = cumulus.NewFieldValue(ss[1])
+			}
+
+			assert.Equalf(t, tt.want, got(fields), "ParseFilter(%v) on %v", tt.args, tt.fields)
+		})
+	}
 }
